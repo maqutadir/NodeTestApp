3684e81374af8006f1bc3d8073f913481e3672de
Syed Maqutadir Rehman <maqutadir.rehman@gmail.com>
2022-03-30 15:06:13 -0400

    Tested a simple express route

--- TestFiles/dP.js
+++ TestFiles/dP.js
@@ -1,8 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+const obj1 = { name: "s", age: 25 };
+
 const dRegex = (dString) => {
   dString = dString.split(/\(,\)/g);
   console.log("dString is", dString);
   return dString;
 };
+
+router.get("/", (req, res) => {
+  res.send(`Hello there ${JSON.stringify(obj1)}`);
+  console.log("You are here to get dP");
+});
 
 const ParserFunction = (dArray) => {
   for (let d in dArray) {
@@ -11,8 +21,9 @@ const ParserFunction = (dArray) => {
 };
 
 console.log(dRegex("Test disease: Acne!"));
-console.log("Okay that is fine");
+console.log("Okay that is fine!");
 const result = dRegex("Abdominoplasty (Tummy Tuck)");
 
 console.log("Result is", result);
-module.exports = { dRegex };
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const dP = require("./TestFiles/dP");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "jade");
+app.use("/dP", dP);
+
 //create a server object
 app
   .listen(PORT, () => {
-    console.log(`Server started on ${PORT}`);
+    console.log(`Server started on PORT: ${PORT}`);
   })
   .on("error", function (error) {
     throw error;
